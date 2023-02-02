292650ceff385cea06a6de1f3c37187e020339ff
Juho Simojoki <juho.simojoki@outlook.com>
2023-02-02 19:50:25 +0200

    The first projects starting from HelloWorld etc. Getting to more difficult ones quite soon.

---
+++ ageCheck/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ASSIGNMENT:  Write a C# program which asks the user’s age and then prints out if the user is eligible to cast his/her own vote.
+The age limit for casting a vote is 18.
+
+Example output:
+Please input your age: 
+32
+You can cast your vote!
+ */
+
+using System;
+class ageCheck
+{
+    static void Main(String[] args)
+    {
+        byte age;
+        Console.WriteLine("Please input your age: ");
+        age = Convert.ToByte(Console.ReadLine());
+
+        if(age > 17)
+        {
+            Console.WriteLine("You can cast your vote!");
+        }
+        else 
+        {
+            Console.WriteLine("You're too young!");
+        }
+
+
+        
+    }
+}
---
+++ numberComparison/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Write a program which asks the user to input two integers and then tells which of the numbers is greater or if they are equal.
+
+Example output:
+Please input the first number: 
+42
+Please input the second number: 
+42
+Numbers are equal.
+ */
+
+using System;
+
+class NumberComparison
+{
+    static void Main()
+    {
+        int firstNumber;
+        int secondNumber;
+
+        Console.WriteLine("Please input the first number:");
+        firstNumber = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Please input the second number:");
+        secondNumber = Convert.ToInt32(Console.ReadLine());
+
+        if (firstNumber == secondNumber)
+        {
+            Console.WriteLine("Numbers are equal.");
+        }
+        else if (firstNumber > secondNumber)
+        {
+            Console.WriteLine("Number 1 is greater than number 2.");
+        }
+        else
+        {
+            Console.WriteLine("Number 2 is greater than number 1.");
+        }
+    }
+}
